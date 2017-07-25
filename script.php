6f413ba9c775b1cc9b024f75e4226fd237d4ea2e
Damien <damienlenoir_78@msn.com>
2017-07-25 16:02:27 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,39 +1,34 @@
-<html>
-  <head>
-    <title>Mairies</title>
-  </head>
-  <body>
+
 <?php
 ini_set('max_execution_time', 90000); 
 try{  $bdd = new PDO('mysql:host=localhost;dbname=mairies;charset=utf8', 'root', ''); } catch(Exception $e){  die('Erreur : '.$e->getMessage());} 
 
 $csv = array();
-$lines = file('49to54224.csv', FILE_IGNORE_NEW_LINES);
+$lines = file('csvfile.csv', FILE_IGNORE_NEW_LINES);
 $reponse = array();
-$apikey = "AIzaSyDY3s4T-Kf6FDDF0C_TDbdmWmIRd31hcDY";
+$apikey = "GoogleAPI_key";
 
 foreach ($lines as $key => $value)
 {
     $csv[$key] = str_getcsv($value);
 	$csv[$key] = explode(";", $csv[$key][0]);
-	$cp = $csv[$key][4];
-	$commune = $csv[$key][5];
-	$code_insee = $csv[$key][0];
+	$parameter = $csv[$key][4];
+	$parameter2 = $csv[$key][5];
 	$commune = str_replace(" ", "+", $commune);
-	$query = 'https://maps.googleapis.com/maps/api/geocode/json?address=mairie+de+'.$commune.'+'.$cp.'&key='.$apikey ; 
+	$query = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$parameter.'+'.$parameter2.'&key='.$apikey ; 
+	
 	$json = file_get_contents($query);  
 	$reponse = json_decode($json);
+	
 	$nom = $reponse->results['0']->formatted_address;
 	$latitude = $reponse->results['0']->geometry->location->lat;
 	$longitude = $reponse->results['0']->geometry->location->lng;
+	
 	$bdd->exec('
-		INSERT INTO mairies 
-		VALUES (null, "'.$code_insee.'", "'.$nom.'","'.$latitude.'", "'.$longitude.'");
-		
+		INSERT INTO table 
+		VALUES (values, values);
 	');
 	echo $query;
 }  
 echo '<h1>Finished ! </h1>';
 ?>
-  </body>
-</html>
